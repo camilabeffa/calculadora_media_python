a6ad283cee9f1fc221478c93ddd7055302d6250b
camilabeffa <camilabeffa12@gmail.com>
2026-09-23 22:12:07 -0300

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# calculadora_media_python
-Código Python que calcula a média entre duas notas
---
+++ app.py
@@ -0,0 +1,16 @@
+# projeto exemplo: calculadora de media do aluno
+def calcular_media(nota1, nota2):
+    return (nota1 + nota2) / 2
+
+print("=== sistema de notas do aluno ===")
+n1 = float(input("digite a primeira nota: "))
+n2 = float(input("digite a segunda nota: "))
+media = calcular_media( n1, n2)
+print(f" a media final é: {media:.2f} ")
+if media >= 7.0:
+    print("status: APROVADO!")
+else:
+    print("status: REPROVADO.")
+
+
+
